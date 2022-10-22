96809fd757a2eec94fa794ae2a3e6779e31f1bb5
JackyXu1983 <116427238+JackyXu1983@users.noreply.github.com>
2022-10-22 11:37:49 +0200

    Initial Commit

---
+++ Practise1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practise1
+//
+//  Created by EllaYang on 2022/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
