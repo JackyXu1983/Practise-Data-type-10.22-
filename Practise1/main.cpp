283d30cc8733fbae9542f7d5134ac6d1ef7128ca
JackyXu1983 <116427238+JackyXu1983@users.noreply.github.com>
2022-10-22 11:45:54 +0200

    Update main.cpp

--- Practise1/main.cpp
+++ Practise1/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    float a = 0.302;
+    float b = 128.101;
+    double c = 123;
+    float d = 112.64E3;
+    double e = 0.7623e-2;
+    float f = 0.23002230098;
+    printf("a=%e \nb=%f \nc=%lf \nd=%lE \ne=%lf \nf=%f\n",a,b,c,d,e,f);
+//    std::cout << "Hello, World!\n";
     return 0;
 }
